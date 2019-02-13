d06c50438ffeac43ab51cca84fb055ec2e3490dc
tsyama <tsyama.desu@gmail.com>
2019-02-13 23:34:32 +0900

    ロギング設定の追加

--- web-data/index.php
+++ web-data/index.php
@@ -6,8 +6,18 @@ require_once 'vendor/autoload.php';
 $config = require_once 'config.php';
 
 $app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $fileHandler = new \Monolog\Handler\StreamHandler('logs/app.log');
+    $logger->pushHandler($fileHandler);
+    return $logger;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
+    $this->logger->addInfo("Hello, {$name}");
     $response->getBody()->write("Hello, $name");
 
     return $response;
