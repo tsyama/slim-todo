9ec3f961de4f2a3f5bf1237437c5db0dbfd9f532
tsyama <tsyama.desu@gmail.com>
2019-02-16 03:02:49 +0900

    cakephp/ormを導入

--- web-data/config.php
+++ web-data/config.php
@@ -3,12 +3,6 @@
 return [
     'displayErrorDetails' => true,
     'addContentLengthHeader' => false,
-    'db' => [
-        'host' => 'db',
-        'user' => 'root',
-        'password' => 'root',
-        'dbname' => 'slim_todo',
-    ],
     'renderer' => [
         'blade_template_path' => __DIR__ . '/views',
         'blade_cache_path' => __DIR__ . '/cache',
--- web-data/index.php
+++ web-data/index.php
@@ -2,8 +2,10 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Slim\Views\Blade;
+use \Cake\ORM\Locator\TableLocator;
 
 require_once 'vendor/autoload.php';
+require_once 'database.php';
 $config = require_once 'config.php';
 
 $app = new \Slim\App(['settings' => $config]);
@@ -17,16 +19,26 @@ $container['view'] = function ($c) {
     );
 };
 
-$container['logger'] = function($c) {
+$container['logger'] = function ($c) {
     $logger = new \Monolog\Logger('my_logger');
     $fileHandler = new \Monolog\Handler\StreamHandler('logs/app.log');
     $logger->pushHandler($fileHandler);
     return $logger;
 };
 
+$container['locator'] = function ($c) {
+    $locator = new TableLocator();
+    return $locator;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $this->logger->addInfo("Hello, {$name}");
+
+    $ticketTable = $this->locator->get('Tickets');
+    $ticket = $ticketTable->newEntity(['title' => $name]);
+    $ticketTable->save($ticket);
+
     $data = [
         'name' => $name,
     ];
